b9ae0f3465826c9c0c5453aa5bd996267202f88b
Lilitha Mdlalana <jranonymous517@gmail.com>
2022-12-08 16:18:34 +0200

    Fix: config file environment variables

--- composables/useSupabase.ts
+++ composables/useSupabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 const useSupabase = () => {
+  const runtimeConfig = useRuntimeConfig()
   const supabaseUrl = "https://rtjpurjprnjxaqqksglu.supabase.co";
-  const supabaseAPIKey = process.env.SUPABASE_KEY;
+  const supabaseAPIKey = runtimeConfig.public.SUPABASE_KEY;
   const supabase = createClient(supabaseUrl, supabaseAPIKey);
   return { supabase };
 };
